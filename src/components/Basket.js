b0aa97218f8f07edf0e30dbd2cca93b948a5031d
JackScottow <j.scottow@googlemail.com>
2022-12-18 21:14:49 +0000

    responsive

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -17,25 +17,45 @@ const Basket = ({ basket, plusQty, minusQty, clearBasket, removeItem }) => {
         basket.map((item, i) => (
           <div className="basket-item shadow" key={item.id}>
             <div className="basket-item-product-details">
-              <img src={`https://www.themoviedb.org/t/p/w92${item.image}`} alt={item.title} />
-              <Link to={`/movie/${item.title.split(" ").join("")}`} key={item.id} state={item.id} className="basket-item-link">
+              <img
+                src={`https://www.themoviedb.org/t/p/w92${item.image}`}
+                alt={item.title}
+              />
+              <Link
+                to={`/movie/${item.title.split(" ").join("")}`}
+                key={item.id}
+                state={item.id}
+                className="basket-item-link"
+              >
                 {item.title}
               </Link>
             </div>
             <div className="basket-item-cost">
               Price £{item.price}
               <div className="basket-adjust">
-                <Button variant="danger" onClick={() => minusQty(item)} className="basket-adjust-button">
+                <Button
+                  variant="danger"
+                  onClick={() => minusQty(item)}
+                  className="basket-adjust-button"
+                >
                   -
                 </Button>
                 <input type="text" value={item.qty} readonly className="qty" />
-                <Button variant="success" onClick={() => plusQty(item)} className="basket-adjust-button">
+                <Button
+                  variant="success"
+                  onClick={() => plusQty(item)}
+                  className="basket-adjust-button"
+                >
                   +
                 </Button>
               </div>
               <div>Total £{(item.qty * item.price).toFixed(2)}</div>
               <div>
-                <Button variant="danger" onClick={() => removeItem(item)} className="basket-adjust-button">
+                <Button
+                  variant="danger"
+                  onClick={() => removeItem(item)}
+                  className="basket-adjust-button"
+                >
                   <FontAwesomeIcon icon={faTrashCan} />
                 </Button>
               </div>
@@ -47,16 +67,21 @@ const Basket = ({ basket, plusQty, minusQty, clearBasket, removeItem }) => {
       )}
       {basket.length > 0 ? (
         <div className="basket-total shadow">
-          <Button variant="danger" onClick={() => clearBasket()} className="basket-button">
+          <Button
+            variant="danger"
+            onClick={() => clearBasket()}
+            className="basket-button"
+          >
             <FontAwesomeIcon icon={faTrashCan} />
             {`Empty Basket`}
           </Button>
-          <Button href="/checkout" variant="success" className="basket-button" basket={basket}>
+          <Button href="/checkout" variant="success" className="basket-button">
             <FontAwesomeIcon icon={faCreditCard} />
             {`Checkout`}
           </Button>
           <p>
-            Total <span className="total-cost">£{(basketQty * 2.99).toFixed(2)}</span>
+            Total{" "}
+            <span className="total-cost">£{(basketQty * 2.99).toFixed(2)}</span>
           </p>
         </div>
       ) : (
